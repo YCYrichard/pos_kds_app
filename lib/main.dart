736ae29b9e58c2b30edf58c1843ab95b5b6aec42
ricahrdYCY <midknightyeh@hotmail.com>
2026-05-21 19:15:21 +0800

    fix(app): restore compatible main entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'app.dart';
-import 'data/db/app_database.dart';
-import 'data/repositories/menu_repository.dart';
-import 'data/repositories/order_repository.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Debug only:
-  // Uncomment this line when you want to remove the local SQLite DB
-  // and recreate it from scratch on next launch.
-  // await AppDatabase.resetDatabase();
-
-  final menuRepository = MenuRepository();
-  await menuRepository.seedDefaultMenu();
-
-  runApp(
-    MultiProvider(
-      providers: [import 'app_bootstrap.dart';
+import 'app_bootstrap.dart';
 import 'app_role.dart';
 
 Future<void> main() async {
   await bootstrapApp(AppRole.combined);
-}
-        Provider<MenuRepository>.value(value: menuRepository),
-        Provider<OrderRepository>(create: (_) => OrderRepository()),
-      ],
-      child: const PosKdsApp(),
-    ),
-  );
 }
